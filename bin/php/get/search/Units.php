167746df982c561e516c4ea72e91a0178dd31449
rajaveernapu <rajaveernapu@gmail.com>
2021-11-30 00:24:41 +0530

    add the actual query of unit search remove old one

--- bin/php/get/search/Units.php
+++ bin/php/get/search/Units.php
@@ -3,66 +3,76 @@ if( session_id( ) == '' || !isset($_SESSION)) {
     session_start( [ 'read_and_close' => true ] );
     require( '/var/www/html/Portal.Branch.Local/bin/php/index.php' );
 }
-if( isset( $_SESSION[ 'User' ], $_SESSION[ 'Hash' ] ) ){
-    $r = \singleton\database::getInstance( )->query(
-        null,
-        "   SELECT  *
-          FROM    Connection
-          WHERE   Connection.Connector = ?
-                  AND Connection.Hash = ?;",
+if( isset( $_SESSION[ 'Connection' ][ 'User' ], $_SESSION[ 'Connection' ][ 'Hash' ] ) ){
+    //Connection
+    $result = \singleton\database::getInstance( )->query(
+        'Portal',
+        " SELECT  [Connection].[ID]
+        FROM    dbo.[Connection]
+        WHERE       [Connection].[User] = ?
+                AND [Connection].[Hash] = ?;",
         array(
-          $_SESSION[ 'User' ],
-          $_SESSION[ 'Hash' ]
-        )
-      );
-    $Connection = sqlsrv_fetch_array( $r );
-    $User = \singleton\database::getInstance( )->query(
-        null,
-        "   SELECT  Emp.*,
-                    Emp.fFirst AS First_Name,
-                    Emp.Last   AS Last_Name
-            FROM    Emp
-            WHERE   Emp.ID = ?;",
-        array(
-          $_SESSION[ 'User' ]
+            $_SESSION[ 'Connection' ][ 'User' ],
+            $_SESSION[ 'Connection' ][ 'Hash' ]
         )
     );
-    $User = sqlsrv_fetch_array( $User );
-    $r = \singleton\database::getInstance( )->query(
+    $Connection = sqlsrv_fetch_array($result);
+    //User
+    $result = \singleton\database::getInstance( )->query(
         null,
-        "   SELECT  Privilege.Access,
-                    Privilege.Owner,
-                    Privilege.Group,
-                    Privilege.Other
-            FROM    Privilege
-            WHERE   Privilege.User_ID = ?;",
+        " SELECT  Emp.fFirst  AS First_Name,
+                  Emp.Last    AS Last_Name,
+                  Emp.fFirst + ' ' + Emp.Last AS Name,
+                  Emp.Title AS Title,
+                  Emp.Field   AS Field
+          FROM  Emp
+          WHERE   Emp.ID = ?;",
         array(
-          $_SESSION[ 'User' ]
+            $_SESSION[ 'Connection' ][ 'User' ]
+        )
+    );
+    $User   = sqlsrv_fetch_array( $result );
+    //Privileges
+    $Access = 0;
+    $Hex = 0;
+    $result = \singleton\database::getInstance( )->query(
+        'Portal',
+        "   SELECT  [Privilege].[Access],
+                    [Privilege].[Owner], 
+                    [Privilege].[Group], 
+                    [Privilege].[Department],
+                    [Privilege].[Database],
+                    [Privilege].[Server],
+                    [Privilege].[Other],
+                    [Privilege].[Token],
+                    [Privilege].[Internet]
+          FROM      dbo.[Privilege]
+          WHERE     Privilege.[User] = ?;",
+        array(
+            $_SESSION[ 'Connection' ][ 'User' ],
         )
     );
     $Privileges = array();
-    while( $Privilege = sqlsrv_fetch_array( $r ) ){ $Privileges[ $Privilege[ 'Access' ] ] = $Privilege; }
-    $Privileged = False;
-    if( isset( $Privileges[ 'Unit' ] )
-        && $Privileges[ 'Unit' ][ 'Owner' ]  >= 4
-    ){ $Privileged = True; }
-    if(!isset($Connection['ID']) || !$Privileged){print json_encode(array('data'=>array()));}
+    if( $result ){while( $Privilege = sqlsrv_fetch_array( $result, SQLSRV_FETCH_ASSOC ) ){
+
+        $key = $Privilege['Access'];
+        unset( $Privilege[ 'Access' ] );
+        $Privileges[ $key ] = implode( '', array(
+            dechex( $Privilege[ 'Owner' ] ),
+            dechex( $Privilege[ 'Group' ] ),
+            dechex( $Privilege[ 'Department' ] ),
+            dechex( $Privilege[ 'Database' ] ),
+            dechex( $Privilege[ 'Server' ] ),
+            dechex( $Privilege[ 'Other' ] ),
+            dechex( $Privilege[ 'Token' ] ),
+            dechex( $Privilege[ 'Internet' ] )
+        ) );
+    }}
+    if(     !isset( $Connection[ 'ID' ] )
+        ||  !isset( $Privileges[ 'Route' ] )
+        ||  !check( privilege_read, level_group, $Privileges[ 'Route' ] )
+    ){ ?><?php require('404.html');?><?php }
     else {
-
-      $output = array(
-          'sEcho'                 =>  intval( $_GET['draw' ] ),
-          'iTotalRecords'         =>  $iTotal,
-          'iTotalDisplayRecords'  =>  $iFilteredTotal,
-          'aaData'                =>  array( ),
-          'options'               =>  array( )
-      );
-
-    $_GET['iDisplayStart'] = isset($_GET['start']) ? $_GET['start'] : 0;
-    $_GET['iDisplayLength'] = isset($_GET['length']) ? $_GET['length'] : '15';
-
-    $Start = $_GET['iDisplayStart'];
-    $Length = $_GET['iDisplayLength'];
-    $End = $Length == '-1' ? 999999 : intval($Start) + intval($Length);
 
     $conditions = array( );
     $search = array( );
@@ -75,7 +85,7 @@ if( isset( $_SESSION[ 'User' ], $_SESSION[ 'Hash' ] ) ){
     if( isset($_GET[ 'Name' ] ) && !in_array( $_GET[ 'Name' ], array( '', ' ', null ) ) ){
       $parameters[] = $_GET['Name'];
       $parameters[] = $_GET['Name'];
-      $conditions[] = "( Unit.State LIKE '%' + ? + '%' OR Unit.Unit LIKE '%' + ? + '%' )";
+      $conditions[] = "( Unit.City_ID LIKE '%' + ? + '%' OR Unit.Building_ID LIKE '%' + ? + '%' )";
     }
     if( isset($_GET[ 'Customer' ] ) && !in_array( $_GET[ 'Customer' ], array( '', ' ', null ) ) ){
       $parameters[] = $_GET['Customer'];
@@ -102,10 +112,10 @@ if( isset( $_SESSION[ 'User' ], $_SESSION[ 'Hash' ] ) ){
     /*Search Filters*/
     if( isset( $_GET[ 'search' ] ) ){
 
-            $search[] = " Unit.State LIKE '%' + ? + '%'";
+            $search[] = " Unit.City_ID LIKE '%' + ? + '%'";
             $parameters[] = $_GET[ 'search' ];
 
-            $search[] = " Unit.Unit LIKE '%' + ? + '%'";
+            $search[] = " Unit.Building_ID LIKE '%' + ? + '%'";
             $parameters[] = $_GET[ 'search' ];
 
             $search[] = "Customer.Name LIKE '%' + ? + '%'";
@@ -119,16 +129,13 @@ if( isset( $_SESSION[ 'User' ], $_SESSION[ 'Hash' ] ) ){
 
         }
 
-    /*Concatenate Filters*/
-    $conditions = $conditions == array( ) ? "NULL IS NULL" : implode( ' AND ', $conditions );
-    $search     = $search     == array( ) ? "NULL IS NULL" : implode( ' OR ', $search );
+        $conditions = $conditions == array( ) ? "NULL IS NULL" : implode( ' AND ', $conditions );
+        $search     = $search     == array( ) ? "NULL IS NULL" : implode( ' OR ', $search );
 
-    /*ROW NUMBER*/
-    $parameters[] = isset( $_GET[ 'start' ] ) && is_numeric( $_GET[ 'start' ] ) ? $_GET[ 'start' ] -25 : 0;
-    $parameters[] = isset( $_GET[ 'length' ] ) && is_numeric( $_GET[ 'length' ] ) && $_GET[ 'length' ] != -1 ? $_GET[ 'start' ] + $_GET[ 'length' ] + 25 : 0;
-
-    $parameters[] = $End;
-    $Columns = array(
+        $parameters[] = isset( $_GET[ 'start' ] ) && is_numeric( $_GET[ 'start' ] ) ? $_GET[ 'start' ] : 0;
+        $parameters[] = isset( $_GET[ 'length' ] ) && is_numeric( $_GET[ 'length' ] ) && $_GET[ 'length' ] != -1 ? $_GET[ 'start' ] + $_GET[ 'length' ] + 10 : 25;
+        $parameters[] = $_GET[ 'search' ];
+        $Columns = array(
       0 =>  'Unit.ID',
       1 =>  'Unit.State',
       2 =>  'Loc.Tag',
@@ -137,12 +144,13 @@ if( isset( $_SESSION[ 'User' ], $_SESSION[ 'Hash' ] ) ){
       5 =>  'Unit.Status'
     );
 
-    $Order = isset( $Columns[ $_GET['order']['column'] ] )
-        ? $Columns[ $_GET['order']['column'] ]
-        : "Unit.ID";
-    $Direction = in_array( $_GET['order']['dir'], array( 'asc', 'desc', 'ASC', 'DESC' ) )
-      ? $_GET['order']['dir']
-      : 'ASC';
+        $Direction = 'ASC';
+        $Order = isset( $_GET['order']['column'] ) ? $Columns[ $_GET['order']['column'] ] : "[Unit].[ID]";
+        if(isset($_GET['order']['dir'])){
+            $Direction = in_array( $_GET['order']['dir'], array( 'asc', 'desc', 'ASC', 'DESC' ) )
+                ? $_GET['order']['dir']
+                : 'ASC';
+        }
 
       $sQuery = " SELECT 		Top 10
                 tbl.FieldName,
@@ -158,18 +166,17 @@ if( isset( $_SESSION[ 'User' ], $_SESSION[ 'Hash' ] ) ){
 
                      ( SELECT  ROW_NUMBER() OVER (ORDER BY {$Order} {$Direction}) AS ROW_COUNT,
                              Unit.ID AS ID,
-                             Unit.State + ' - ' + Unit.Unit AS Name,
-                             CASE WHEN Unit.State IN ( null, ' ', '  ' ) THEN 'Untitled' ELSE Unit.State END AS City_ID,
+                             Unit.City_ID,
                              Customer.ID AS Customer_ID,
                              Customer.Name AS Customer_Name,
                              Location.Loc AS Location_ID,
                              Location.Tag AS Location_Name,
-                             Unit.Unit AS Building_ID,
+                             Unit.Building_ID AS Building_ID,
                              Unit.Type AS Type,
                              Unit.Status AS Status,
                              Ticket.ID AS Ticket_ID,
                              Ticket.Date AS Ticket_Date
-                     FROM    Elev AS Unit
+                     FROM    Unit
                              LEFT JOIN Loc AS Location ON Unit.Loc = Location.Loc
                              LEFT JOIN (
                                SELECT  Owner.ID,
@@ -193,7 +200,7 @@ if( isset( $_SESSION[ 'User' ], $_SESSION[ 'Hash' ] ) ){
       GROUP BY 	tbl.FieldName, tbl.FieldValue;";
     //echo $sQuery;
     //var_dump( $parameters );
-    $rResult = \singleton\databse::getInstance()->query(
+        $rResult = $database->query(
       null,
       $sQuery,
       $parameters
