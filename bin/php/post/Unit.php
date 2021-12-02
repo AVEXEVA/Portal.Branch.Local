b8098f769256e250554329d4bda6fde2b530342e
AVEXEVA <thinksperanza@gmail.com>
2021-12-02 10:39:10 -0500

    Update 2021-12-2 10:39:00.000

--- bin/php/post/Unit.php
+++ bin/php/post/Unit.php
@@ -1,80 +1,132 @@
-<?php 
-session_start( [ 'read_and_close' => true ] );
-require('../get/index.php');
-if(isset($_SESSION['User'],$_SESSION['Hash'])){
-    $r = $database->query(null,"SELECT * FROM Connection WHERE Connector = ? AND Hash = ?;",array($_SESSION['User'],$_SESSION['Hash']));
-    $array = sqlsrv_fetch_array($r);
-    $Privileged = FALSE;
-    if(!isset($_SESSION['Branch']) || $_SESSION['Branch'] == 'Nouveau Elevator'){
-        $r = $database->query(null,"SELECT * FROM Emp WHERE ID = ?",array($_GET['User']));
-        $My_User = sqlsrv_fetch_array($r);
-        $Field = ($User['Field'] == 1 && $User['Title'] != "OFFICE") ? True : False;
-        $r = $database->query($Portal,"
-            SELECT Access, Owner, Group, Other
-            FROM   Privilege
-            WHERE  User_ID = ?
-        ;",array($_SESSION['User']));
-        $My_Privileges = array();
-        while($array2 = sqlsrv_fetch_array($r)){$My_Privileges[$array2['Access']] = $array2;}
-        $Privileged = FALSE;
-        if(isset($My_Privileges['Unit']) && $My_Privileges['Unit']['Owner'] >= 6 && $My_Privileges['Unit']['Group'] >= 6 && $My_Privileges['Unit']['Other'] >= 6){$Privileged = TRUE;}
-    }
-    if(!$Privileged || count($_POST) == 0){?><html><head><script>document.location.href='../login.php';</script></head></html><?php }
+<?php
+if( session_id( ) == '' || !isset($_SESSION)) {
+    session_start( [ 'read_and_close' => true ] );
+    require( '/var/www/html/Portal.Branch.Local/bin/php/index.php' );
+}
+if( isset( $_SESSION[ 'Connection' ][ 'User' ], $_SESSION[ 'Connection' ][ 'Hash' ] ) ){
+    //Connection
+    $result = \singleton\database::getInstance( )->query(
+        'Portal',
+        " SELECT  [Connection].[ID]
+        FROM    dbo.[Connection]
+        WHERE       [Connection].[User] = ?
+                AND [Connection].[Hash] = ?;",
+        array(
+            $_SESSION[ 'Connection' ][ 'User' ],
+            $_SESSION[ 'Connection' ][ 'Hash' ]
+        )
+    );
+    $Connection = sqlsrv_fetch_array($result);
+    //User
+    $result = \singleton\database::getInstance( )->query(
+        null,
+        " SELECT  Emp.fFirst  AS First_Name,
+		          Emp.Last    AS Last_Name,
+		          Emp.fFirst + ' ' + Emp.Last AS Name,
+		          Emp.Title AS Title,
+		          Emp.Field   AS Field
+		  FROM  Emp
+		  WHERE   Emp.ID = ?;",
+        array(
+            $_SESSION[ 'Connection' ][ 'User' ]
+        )
+    );
+    $User   = sqlsrv_fetch_array( $result );
+    //Privileges
+    $Access = 0;
+    $Hex = 0;
+    $result = \singleton\database::getInstance( )->query(
+        'Portal',
+        "   SELECT  [Privilege].[Access],
+                    [Privilege].[Owner],
+                    [Privilege].[Group],
+                    [Privilege].[Department],
+                    [Privilege].[Database],
+                    [Privilege].[Server],
+                    [Privilege].[Other],
+                    [Privilege].[Token],
+                    [Privilege].[Internet]
+		  FROM      dbo.[Privilege]
+		  WHERE     Privilege.[User] = ?;",
+        array(
+            $_SESSION[ 'Connection' ][ 'User' ],
+        )
+    );
+    $Privileges = array();
+    if( $result ){while( $Privilege = sqlsrv_fetch_array( $result, SQLSRV_FETCH_ASSOC ) ){
+        $key = $Privilege['Access'];
+        unset( $Privilege[ 'Access' ] );
+        $Privileges[ $key ] = implode( '', array(
+            dechex( $Privilege[ 'Owner' ] ),
+            dechex( $Privilege[ 'Group' ] ),
+            dechex( $Privilege[ 'Department' ] ),
+            dechex( $Privilege[ 'Database' ] ),
+            dechex( $Privilege[ 'Server' ] ),
+            dechex( $Privilege[ 'Other' ] ),
+            dechex( $Privilege[ 'Token' ] ),
+            dechex( $Privilege[ 'Internet' ] )
+        ) );
+    }}
+    if( 	!isset( $Connection[ 'ID' ] )
+        ||  !isset( $Privileges[ 'Customer' ] )
+        || 	!check( privilege_delete, level_group, $Privileges[ 'User' ] )
+    ){ ?><?php require('404.html');?><?php }
     else {
-		if(isset($_POST['action']) && $_POST['action'] == 'edit'){
-			if(isset($_POST['data']) && count($_POST['data']) > 0){
-				$data = array();
-				foreach($_POST['data'] as $ID=>$Unit){
-					$resource = $database->query(null,"
-						SELECT Loc.Loc AS Location_ID
-						FROM   nei.dbo.Loc
-						WHERE  Loc.Tag = ?
-					;",array($Unit['Location']));
-					if($r){$Location_ID = sqlsrv_fetch_array($resource)['Location_ID'];}
-					$database->query(null,"
-						UPDATE nei.dbo.Elev
-						SET    Elev.State   = ?,
-						 	   Elev.Unit    = ?,
-							   Elev.Loc     = ?,
-							   Elev.Type    = ?,
-							   Elev.fDesc   = ?,
-							   Elev.Status  = ?
-						WHERE  Elev.ID      = ?
-					;", array($Unit['State'], $Unit['Unit'], $Location_ID, $Unit['Type'], $Unit['Description'], $Unit['Status'], $ID));
-					$Unit['ID'] = intval($ID);
-					$data[] = $Unit;
-				}
-				print json_encode(array("data"=>$data));
-			}
-		} elseif(isset($_POST['action']) && $_POST['action'] == 'create'){
-			if(isset($_POST['data']) && count($_POST['data']) > 0){
-				$data = array();
-				foreach($_POST['data'] as $ID=>$Unit){
-					$resource = $database->query(null,"
-						SELECT Loc.Loc AS Location_ID
-						FROM   nei.dbo.Loc
-						WHERE  Loc.Tag = ?
-					;",array($Unit['Location']));
-					if($r){$Location_ID = sqlsrv_fetch_array($resource)['Location_ID'];}
-					$resource = $database->query(null,"SELECT Max(Elev.ID) AS ID FROM nei.dbo.Elev;");
-					$Unit_Primary_Key = sqlsrv_fetch_array($resource)['ID'];
-					$Unit_Primary_Key++;
-					$resource = $database->query(null,"
-						INSERT INTO Elev(ID, Unit, State, Loc, Owner, Cat, Type, Status)
-						VALUES(?,?,?,?,?,?,?,?)
-					;",array($Unit_Primary_Key, $Unit['Unit'], $Unit['State'], $Location_ID, $_GET['ID'], 'Public', $Unit['Type'], $Unit['Status']));
-					if( ($errors = sqlsrv_errors() ) != null) {
-						foreach( $errors as $error ) {
-							echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
-							echo "code: ".$error[ 'code']."<br />";
-							echo "message: ".$error[ 'message']."<br />";
-						}
-					}
-					$Unit['ID'] = $Unit_Primary_Key;
-					$data[] = $Unit;
-				}
-				print json_encode(array("data"=>$data));
-			}
-		}
+        \singleton\database::getInstance( )->query(
+            null,
+            " INSERT INTO Activity([User], [Date], [Page] )
+            VALUES( ?, ?, ? );",
+            array(
+                $_SESSION[ 'Connection' ][ 'User' ],
+                date('Y-m-d H:i:s'),
+                'post/customer.php'
+            )
+        );
+        if(isset($_POST['action']) && $_POST['action'] == 'delete'){
+            if(isset($_POST['data']) && count($_POST['data']) > 0){
+                foreach($_POST['data'] as $ID){
+                    $database->query(
+                        null,
+                        "	DELETE FROM dbo.[Unit] 
+							WHERE 		[Unit].[ID] = ?;",
+                        array(
+                            $ID
+                        )
+                    );
+                }
+                print json_encode(array('data'=>array()));
+            }
+        }
+        elseif(isset($_POST['action']) && $_POST['action'] == 'create'){
+            if(isset($_POST['data']) && count($_POST['data']) > 0){
+                $data = array();
+                foreach($_POST['data'] as $ID){
+                    $r = $database->query(null,"
+						SELECT *
+						FROM   dbo.[Unit] 
+						WHERE  [Unit].[ID] = ?
+					;",array($ID));
+                    if($r){$Unit = sqlsrv_fetch_array($r);}
+
+                    $resource = $database->query(null,"SELECT Max(Unit.ID) AS ID FROM demo.dbo.Unit;");
+                    $Unit_Primary_Key = sqlsrv_fetch_array($resource)['ID'];
+                    $Unit_Primary_Key++;
+                    $resource = $database->query(null,"
+						INSERT INTO Unit(ID, fDesc, Remarks, Building_ID, City_ID, Loc, Cat, Type, Location_Category, Manuf, Install, InstallBy, Since, Last, Price, Owner, fGroup, Serial, Template, Status, TFMID, TFMSource)
+						VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+					;",array($Unit_Primary_Key, $Unit['fDesc'], $Unit['Remarks'],$Unit['Building_ID'],$Unit['City_ID'],$Unit['Loc'],$Unit['Cat'],$Unit['Type'],$Unit['Location_Category'],$Unit['Manuf'],$Unit['Install'],$Unit['InstallBy'],$Unit['Since'],$Unit['Last'],$Unit['Price'],$Unit['Owner'],$Unit['fGroup'],$Unit['Serial'],$Unit['Template'],$Unit['Status'],$Unit['TFMID'],$Unit['TFMSource']));
+                    if( ($errors = sqlsrv_errors() ) != null) {
+                        foreach( $errors as $error ) {
+                            echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
+                            echo "code: ".$error[ 'code']."<br />";
+                            echo "message: ".$error[ 'message']."<br />";
+                        }
+                    }
+                    $Unit['ID'] = $Unit_Primary_Key;
+                    $data[] = $Unit;
+                }
+                print json_encode(array('data'=>array()));
+            }
+        }
     }
 }?>
