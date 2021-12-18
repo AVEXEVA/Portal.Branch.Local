da8aa326023d751a84995322ea192999068aa34b
rajaveernapu <rajaveernapu@gmail.com>
2021-12-18 19:22:46 +0530

    worked on make file task category_tests.php

--- category_tests.php
+++ category_tests.php
@@ -31,6 +31,7 @@ if( isset( $_SESSION[ 'Connection' ][ 'User' ], $_SESSION[ 'Connection' ][ 'Hash
 		  	$_SESSION[ 'Connection' ][ 'User' ]
 		)
 	);
+
 	$User   = sqlsrv_fetch_array( $result );
 	//Privileges
 	$Access = 0;
@@ -52,6 +53,7 @@ if( isset( $_SESSION[ 'Connection' ][ 'User' ], $_SESSION[ 'Connection' ][ 'Hash
 		  	$_SESSION[ 'Connection' ][ 'User' ],
 		)
 	);
+
     $Privileges = array();
     if( $result ){while( $Privilege = sqlsrv_fetch_array( $result, SQLSRV_FETCH_ASSOC ) ){
 
@@ -68,11 +70,13 @@ if( isset( $_SESSION[ 'Connection' ][ 'User' ], $_SESSION[ 'Connection' ][ 'Hash
         	dechex( $Privilege[ 'Internet' ] )
         ) );
     }}
-    if( 	!isset( $Connection[ 'ID' ] )
+   /* if( 	!isset( $Connection[ 'ID' ] )
         ||  !isset( $Privileges[ 'Category_Test' ] )
         || 	!check( privilege_read, level_group, $Privileges[ 'Category_Test' ] )
-    ){ ?><?php require('404.html');?><?php }
-    else {
+    ){ ?><?php require('404.html');?><?php
+      } 
+    else { */
+       
         \singleton\database::getInstance( )->query(
           null,
           " INSERT INTO Activity([User], [Date], [Page] )
@@ -87,50 +91,44 @@ if( isset( $_SESSION[ 'Connection' ][ 'User' ], $_SESSION[ 'Connection' ][ 'Hash
 <html lang="en">
 <head>
   <title><?php echo $_SESSION[ 'Connection' ][ 'Branch' ];?> | Portal</title>
-     <?php $_GET[ 'Bootstrap' ] = '5.1';?>
-    <?php  $_GET[ 'Entity_CSS' ] = 1;?>
-    <?php	require( bin_meta . 'index.php');?>
-    <?php	require( bin_css  . 'index.php');?>
-      <?php require( bin_js   . 'index.php');?>
+     <?php  $_GET[ 'Bootstrap' ] = '5.1';?>
+     <?php  $_GET[ 'Entity_CSS' ] = 1;?>
+     <?php  require( bin_meta . 'index.php');?>
+     <?php  require( bin_css  . 'index.php');?>
+     <?php  require( bin_js   . 'index.php');?>
 </head>
-<body onload='finishLoadingPage();'>
-    <div id="wrapper">
-        <?php require( bin_php . 'element/navigation.php');?>
-        <?php require( bin_php . 'element/loading.php');?>
-        <div id="page-wrapper" class='content'>
-            <div class="card card-full card-primary border-0">
-                <div class="card-heading"><h4><?php \singleton\fontawesome::getInstance( )->Inspection( 1 );?> Category Tests</h4></div>
-                <div class="form-mobile card-body bg-dark text-white"><form method='GET' action='category_tests.php'>
-                  <div class='row'><div class='col-12'>&nbsp;</div></div>
-                  <div class='row'>
-                      <div class='col-4'>Search:</div>
-                      <div class='col-8'><input type='text' name='Search' placeholder='Search' class='redraw' /></div>
-                  </div>
-                  <div class='row'><div class='col-12'>&nbsp;</div></div>
-                  <div class='row'>
-                    <div class='col-4'>ID:</div>
-                    <div class='col-8'><input type='text' name='ID' placeholder='ID' class='redraw' value='<?php echo $_GET[ 'ID' ];?>' /></div>
-                  </div>
-                  <div class='row'>
-                    <div class='col-4'>Name:</div>
-                    <div class='col-8'><input type='text' name='Name' placeholder='Name' class='redraw' value='<?php echo $_GET[ 'Name' ];?>' /></div>
-                  </div>
-                </form></div>
-                <div class="card-body bg-dark">
-                    <table id='Table_Category_Tests' class='display' cellspacing='0' width='100%'>
-                        <thead><tr class='text-center'>
-                            <th class='text-white border border-white' title='ID'>ID</th>
-                            <th class='text-white border border-white' title='Name'>Name</th>
-                        </tr><tr class='form-desktop'>
-                            <th class='text-white border border-white' title='ID'><input class='redraw form-control' type='text' name='ID' value='<?php echo isset( $_GET[ 'ID' ] ) ? $_GET[ 'ID' ] : null; ?>' /></th>
-                            <th class='text-white border border-white' title='Name'><input class='redraw form-control' type='text' name='Name' value='<?php echo isset( $_GET[ 'Name' ] ) ? $_GET[ 'Name' ] : null; ?>' /></th>
-                        </tr></thead>
-                    </table>
-                </div>
-            </div>
+<body>
+  <div id="wrapper">
+    <?php require( bin_php . 'element/navigation.php');?>
+    <div id="page-wrapper" class='content'>
+      <div class="card card-full card-primary border-0">
+        <form method='GET' action='category_tests.php'>
+          <div class="card-heading"><h4><?php \singleton\fontawesome::getInstance( )->Inspection( 1 );?> Category Tests</h4></div>
+          <div class="mobile card-body bg-dark text-white"><?php 
+            \singleton\bootstrap::getInstance( )->card_row_form_input( 'Search', isset( $_GET[ 'Search' ] ) ? $_GET[ 'Search' ] : null, false, false, false, 'redraw' );
+            \singleton\bootstrap::getInstance( )->card_row_form_input( 'ID', isset( $_GET[ 'ID' ] ) ? $_GET[ 'ID' ] : null, false, false, false, 'redraw' );
+            \singleton\bootstrap::getInstance( )->card_row_form_input( 'Name', isset( $_GET[ 'Name' ] ) ? $_GET[ 'Name' ] : null, false, false, false, 'redraw' );
+            
+          ?></div>
+          <div class="card-body bg-dark">
+            <table id='Table_Category_Tests' class='display' cellspacing='0' width='100%'>
+              <thead><tr class='text-center'><?php 
+                \singleton\table::getInstance( )->th( 'ID', 'ID' );
+                \singleton\table::getInstance( )->th( 'Name', 'Name' );
+                
+              ?></tr><tr class='form-desktop'><?php 
+                \singleton\table::getInstance( )->th_input( 'ID', isset( $_GET[ 'ID' ] ) ? $_GET[ 'ID' ] : null );
+                \singleton\table::getInstance( )->th_input( 'Name', isset( $_GET[ 'Name' ] ) ? $_GET[ 'Name' ] : null );
+               
+              ?></tr></thead>
+            </table>
+          </div>
         </div>
+      </form>
     </div>
+  </div>
 </body>
 </html>
-<?php }
+
+<?php /*}*/
 } else {?><script>document.location.href='../login.php?Forward=Category_Tests.php&<?php echo http_build_query( $_GET );?>';</script><?php }?>
